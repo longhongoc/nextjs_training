72108562b7837becafe90d0c4ba5e55c3b6a4341
longbin25 <hongoclong250802@gmail.com>
2025-09-25 14:53:46 +0700

    fix invoice delete button

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -97,9 +97,12 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error("Failed to Delete Invoice")
+  try{
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
+  } catch (error){
+    throw new Error("Failed to Delete Invoice." + error)
+  }
 }
 
 export type State = {
