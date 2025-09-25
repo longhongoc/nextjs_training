b238d54a90f62f2186f99be4ed900d83778d20e1
longbin25 <hongoclong250802@gmail.com>
2025-09-25 14:10:06 +0700

    using-lint-to-check-and-fix

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -47,6 +47,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
+    console.error(error)
     // If a database error occurs, return a more specific error.
     return {
       message: 'Database Error: Failed to Create Invoice.',
@@ -88,7 +89,7 @@ export async function updateInvoice(
       WHERE id = ${id}
     `;
   } catch (error) {
-    return { message: 'Database Error: Failed to Update Invoice.' };
+    return { message: `Database Error: Failed to Update Invoice. ${error}` };
   }
  
   revalidatePath('/dashboard/invoices');
